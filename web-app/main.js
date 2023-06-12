4dd17fad0434cdbe50f7d78f123da692465f7b55
YujeongSeo <yujeong.seo22@imperial.ac.uk>
2023-06-12 10:50:58 +0100

    Commit: token placing

--- web-app/main.js
+++ web-app/main.js
@@ -1,24 +1,32 @@
 import R from "./ramda.js";
 
 const game_board = document.getElementById("game_board");
-
-// initialise the board as 2D array
 const initial_board = new Array(15).fill().map(() => new Array(10).fill(""));
 
+let token = "a_token";
+
 function create_board () {
-    for (let i = 0; i < initial_board.length; i++) {
-        for (let j = 0; j < initial_board[i].length; j++) {
+    initial_board.forEach (function(row, i) {
+        row.forEach (function(_item, j) {
             const grid_element = document.createElement("div");
+            grid_element.id = `${i}-${j}`;
             grid_element.classList.add("grid");
-            grid_element.id = `${i},${j}`;
-            grid_element.addEventListener("click", add_token);
             game_board.append(grid_element);
-        }
-      }
+            grid_element.addEventListener("click", add_token);
+        });
+    });
 }
 
-function add_token(e) {
-    console.log(e.target)
+function add_token (e) {
+    const display_token = document.createElement("div");
+    display_token.classList.add(token);
+    e.target.append(display_token);
+    token = (
+        token === "a_token"
+        ? "b_token" // if token is a_token change to b_token
+        : "a_token" // if token is not a_token change to a_token
+    );
+    e.target.removeEventListener("click", add_token);
 }
 
 create_board();
